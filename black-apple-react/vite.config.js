3f112794677e9cce2cd36025ef58a26ccf598d9a
xiao.wenpeng <xwp235@gmail.com>
2026-03-09 12:32:40 +0900

    first commit

---
+++ black-apple-react/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const createAlias = (dir) => path.resolve(__dirname, `src/${dir}`);
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": createAlias(""), // src
+      "@components": createAlias("components"), // src/components
+      "~img": createAlias("assets/images"), // src/assets/images
+      "#types": createAlias("/types"), // src/types
+    },
+  },
+});
